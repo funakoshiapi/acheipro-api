50798cdfabc7b9978ee4d5f8cd5f1544c51bcb19
funakoshisilva <silvakoshis@gmail.com>
2024-04-24 13:11:49 +0100

    Removed requirement

--- Entities/Models/CompanyData.cs
+++ Entities/Models/CompanyData.cs
@@ -12,11 +12,9 @@ namespace Entities.Models
         [Column("CompanyId")]
         public Guid CompanyId { get; set; }
 
-        [Required(ErrorMessage ="Company Description is a required field.")]
 		[MaxLength(1000, ErrorMessage ="Maximum length for the Description is 1000 characters.")]
 		public string? CompanyDescription { get; set; }
 
-        [Required(ErrorMessage = "Company Mission is a required field.")]
         [MaxLength(1000, ErrorMessage = "Maximum length for the Mission is 1000 characters.")]
 		public string? CompanyMission { get; set; }
 
