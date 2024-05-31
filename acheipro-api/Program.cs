d39d89b4cba5f676bdadff9765105283e200dc91
funakoshisilva <silvakoshis@gmail.com>
2024-05-30 21:45:52 -0400

    production changes

--- acheipro-api/Program.cs
+++ acheipro-api/Program.cs
@@ -68,11 +68,11 @@ builder.Services.ConfigureSwagger();
 
 var app = builder.Build();
 
- using (var scope = app.Services.CreateScope())
+ /*using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<RepositoryContext>();
     db.Database.Migrate();
-} 
+} */
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
